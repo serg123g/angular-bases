ff77f8c455a041c1ab6102e11014ccbd8b985dbe
sportugal <patogal@outlook.es>
2024-04-09 14:08:10 -0500

    Finalizacion Seccion 5 angular dbz components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
+
 
 
 
@@ -18,6 +20,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
